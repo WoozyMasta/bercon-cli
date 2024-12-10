69225a06c3c416f037efcb1d6f7e24b4cd5cfdb4
WoozyMasta <kagbe.leviy@gmail.com>
2024-12-11 00:05:57 +0300

    chore: add simple test for beprinter

---
+++ pkg/beprinter/beprinter_test.go
@@ -0,0 +1,26 @@
+package beprinter
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+// LoadTestData reads test data from a specified text file.
+func LoadTestData(filename string) ([]byte, error) {
+	data, err := os.ReadFile(filepath.Join("test_data", filename))
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+// TestParsePlayers tests the ParsePlayers function.
+func TestParsePlayers(t *testing.T) {
+	input, err := LoadTestData("players.txt")
+	if err != nil {
+		t.Fatalf("Failed to load players test data: %v", err)
+	}
+
+	ParseAndPrintData(input, "players", "GeoLite2-Country.mmdb", true)
+}

cd404d073f9682f1e200c9f01a3639f93d7ed1ab
WoozyMasta <kagbe.leviy@gmail.com>
2024-12-10 23:10:48 +0300

    feat: beparser add ParseWithGeo and ParseWithGeoDB

--- pkg/beparser/beparser.go
+++ pkg/beparser/beparser.go
@@ -1,5 +1,11 @@
 package beparser
 
+import (
+	"fmt"
+
+	"github.com/oschwald/geoip2-golang"
+)
+
 const (
 	defaultInvalidGUID = "00000000000000000000000000000000"
 	hashBytesGUID      = 32
@@ -29,4 +35,51 @@ func Parse(data []byte, cmd string) any {
 		message.Parse(data)
 		return message
 	}
+}
+
+// Return struct by passed command like Parse with country from IP fields by geoip2.Reader
+func ParseWithGeo(data []byte, cmd string, geoReader *geoip2.Reader) (any, error) {
+
+	// fallback if geo reader is nil
+	if geoReader == (*geoip2.Reader)(nil) {
+		return Parse(data, cmd), fmt.Errorf("")
+	}
+
+	switch cmd {
+	case "players":
+		players := NewPlayers()
+		players.Parse(data)
+		players.SetCountryCode(geoReader)
+		return players, nil
+	case "bans":
+		bans := NewBans()
+		bans.Parse(data)
+		bans.SetCountryCode(geoReader)
+		return bans, nil
+	case "admins":
+		admins := NewAdmins()
+		admins.Parse(data)
+		admins.SetCountryCode(geoReader)
+		return admins, nil
+	default:
+		message := NewMessage()
+		message.Parse(data)
+		return message, nil
+	}
+}
+
+// Return struct by passed command like Parse with country from IP fields by geoDB file path
+func ParseWithGeoDB(data []byte, cmd string, geoDB string) (any, error) {
+	reader, err := geoip2.Open(geoDB)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func(reader *geoip2.Reader) {
+		if err := reader.Close(); err != nil {
+			panic("Cant close GeoDB")
+		}
+	}(reader)
+
+	return ParseWithGeo(data, cmd, reader)
 }
--- pkg/beparser/geo.go
+++ pkg/beparser/geo.go
@@ -6,28 +6,33 @@ import (
 	"github.com/oschwald/geoip2-golang"
 )
 
+// Set country code from geoDB for admin IP
 func (a *Admins) SetCountryCode(geoDB *geoip2.Reader) {
 	for i := range *a {
 		(*a)[i].Country = getCountryCodeFromIP(geoDB, (*a)[i].IP)
 	}
 }
 
+// Set country code from geoDB for player IP
 func (p *Players) SetCountryCode(geoDB *geoip2.Reader) {
 	for i := range *p {
 		(*p)[i].Country = getCountryCodeFromIP(geoDB, (*p)[i].IP)
 	}
 }
 
+// Set country code from geoDB for IP bans
 func (b *BansIP) SetCountryCode(geoDB *geoip2.Reader) {
 	for i := range *b {
 		(*b)[i].Country = getCountryCodeFromIP(geoDB, (*b)[i].IP)
 	}
 }
 
+// Set country code from geoDB for IP bans in global Bans struct
 func (b *Bans) SetCountryCode(geoDB *geoip2.Reader) {
 	b.IPBans.SetCountryCode(geoDB)
 }
 
+// return country code from geoDB or XX for all unexpected variants
 func getCountryCodeFromIP(geoDB *geoip2.Reader, ip string) string {
 	netIP := net.ParseIP(ip)
 	if netIP == nil {
